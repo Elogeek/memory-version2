ed60cc9cd51ae1055e5ba3dda5e289acc7294c1d
elogeek <elodiechristin@gmail.com>
2021-03-30 16:23:14 +0200

    go

--- app.js
+++ app.js
@@ -4,57 +4,62 @@ for(let i = 0; i < cards.length; i++) {
     console.log(cards.length); //answer 20 it's OK
 }
 
-//face-hidden
-let facePrincess = [
-    "alice.png",
-    "anna.webp",
-    "ariel.webp",
-    "aurora.webp",
-    "blanche-neige.webp",
-    "elsa.webp",
-    "jasmine.webp",
-    "merida.webp",
-    "mulan.webp",
-    "raiponce.webp",
-];
+let paire = 2;
+let score = document.getElementById("points");
 
-//face hidden lol manga (20 cards)
-let faceLol = [
-    "lol-1.jpg",
-    "lol-2.jpg",
-    "lol-3.jpg",
-    "lol-4.jpg",
-    "lol-5.jpg",
-    "lol6.jpg",
-    "lol-7.jpg",
-    "lol8.jpg",
-    "lol-9.jpg",
-    "lol-10.jpg"
-];
+//random cards
+function shuffleCards() {
+    cards.style.backgroundImage = "url(theme[1])";
+    cards.random();
+}
 
-//face hidden big lol (24 cards)
-let bigFace = [
-    "lol-1.jpg",
-    "lol-2.jpg",
-    "lol-3.jpg",
-    "lol-4.jpg",
-    "lol-5.jpg",
-    "lol-6.png",
-    "lol-7.jpg",
-    "lol-8.png",
-    "lol-9.jpg",
-    "lol-10.jpg",
-    "lol-11.jpg",
-    "lol-12.jpg"
-];
+/**
+ * @returns {number}
+ */
+function random() {
+    return Math.trunc(Math.random() * 20);
+}
 
-let score = 0;
-let paire = 0;
+document.querySelector("#newGame").addEventListener("click", startGame);
 
-//choice theme
-let choiceA = "defaut";
-let choiceB = "alternatif";
+//start game
+function startGame() {
+    window.reload();
+   initGame();
+}
+
+//initialize game (conditions : win/ lose)
+function mainGame() {
+    for (let card of cards) {}
+        //2 different cards
 
 
+}
+
+mainGame();
+
+//choice theme and reset game
+function initGame() {
+    let teme = document.getElementById("theme");
+    teme.addEventListener("click", function() {
+
+        teme.value = theme[1];
+        teme.value = theme[2];
+        teme.value = theme[3];
+    })
+
+}
+initGame();
+
+
+function finishGame() {
+    if(score.innerHTML === "10") {
+        alert("Vous avez gagner!");
+        initGame();
+        startGame();
+
+    }
+
+}
 
 
---
+++ theme.js
@@ -0,0 +1,4 @@
+let theme = [];
+theme[1] = {titre: "princess", nbCards: 20, dossier: "princess", extension: "webp"};
+theme[2] = {titre: "LOL", nbCards: 20, dossier: "LOL", extension: "jpg"};
+theme[3] = {title:"lol", nbCards:20, dossier: "lol_manga", extension: "jpg"};
